568a94e8d9400250b2990412e4bb708b94728f9a
darrylnalanda <107294779+darrylnalanda@users.noreply.github.com>
2023-02-09 15:19:37 +0700

    First Commit

---
+++ alphabeticalorder.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	long long int x1, x2, x3, dadu1, dadu2, A, B, C, D;
+	scanf ("%lld %lld %lld", x1, x2, x3);
+	
+	dadu1= x1 x2 x3;
+	dadu2= x1 x2 x3;
+	
+	if (dadu1= B D A){
+		printf("3 2 1\n");
+		return 0;
+	}
+	else (dadu2= A B C){
+		printf("1 2 3\n");
+	}
+		
+	return 0;
+}
